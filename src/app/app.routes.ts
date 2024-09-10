1522f877ab8602f1ce07fde48961a5ee39f471c7
senapathisuresh <senapathisuresh407@gmail.com>
2024-09-10 09:47:10 +0530

    change commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'; // Import for routing
+import { FormsModule } from '@angular/forms'; // Import FormsModule to handle forms
+import { AppComponent } from './app.component';
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'navbar',
+    loadComponent: () => import('./navbar/navbar.component').then(c => c.NavbarComponent) // Lazy load the standalone component
+  },
+  { path: '', redirectTo: '/navbar', pathMatch: 'full' }
+];
+
+
